00a620776350c18a69f24ef01d8b8c91893ac8a5
Tejashwini-Javejar <tejujave2908@gmail.com>
2026-07-02 19:09:01 +0530

    Update Application.java

--- CI-CD /spring-boot-app/src-main/src/main/java/com/app/Application.java
+++ CI-CD /spring-boot-app/src-main/src/main/java/com/app/Application.java
@@ -2,20 +2,9 @@ package com.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
-@Controller
 public class Application {
-
-    @GetMapping("/")
-    public String index(Model model) {
-        model.addAttribute("title", "I have successfully built a Spring Boot application using Maven");
-        model.addAttribute("msg", "This application is deployed on Kubernetes using Argo CD");
-        return "index";
-    }
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
