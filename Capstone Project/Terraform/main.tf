3882965118f149b361bd9e6dc8aed76082c9b12e
Tejashwini-Javejar <tejujave2908@gmail.com>
2026-07-05 13:44:18 +0530

    Add VPC module configuration to main.tf

--- Capstone Project/Terraform/main.tf
+++ Capstone Project/Terraform/main.tf
@@ -1,5 +1,11 @@
-provider "aws" {
+module "vpc" {
 
-  region = var.aws_region
+  source = "./modules/vpc"
+
+  vpc_cidr = var.vpc_cidr
+
+  public_subnets = var.public_subnets
+
+  private_subnets = var.private_subnets
 
 }
