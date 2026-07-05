41381a10e0745a3fd6b15065e1648b4b0dc63317
Tejashwini-Javejar <tejujave2908@gmail.com>
2026-07-05 14:19:08 +0530

    Add outputs for EKS cluster, VPC, and node group

--- Capstone Project/Terraform/outputs.tf
+++ Capstone Project/Terraform/outputs.tf
@@ -1,1 +1,11 @@
+output "cluster_name" {
+  value = aws_eks_cluster.cluster.name
+}
 
+output "vpc_id" {
+  value = aws_vpc.main.id
+}
+
+output "node_group" {
+  value = aws_eks_node_group.nodes.node_group_name
+}
