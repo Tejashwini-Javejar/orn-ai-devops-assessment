1ea4862f3ef16eb36fbd2135c6355ca92caca392
Tejashwini-Javejar <tejujave2908@gmail.com>
2026-07-05 13:53:51 +0530

    Change VPC CIDR to '10.0.0.0/16'
    Updated VPC module configuration with hardcoded CIDR.

--- Capstone Project/Terraform/main.tf
+++ Capstone Project/Terraform/main.tf
@@ -2,10 +2,5 @@ module "vpc" {
 
   source = "./modules/vpc"
 
-  vpc_cidr = var.vpc_cidr
-
-  public_subnets = var.public_subnets
-
-  private_subnets = var.private_subnets
-
+  vpc_cidr = "10.0.0.0/16"
 }
