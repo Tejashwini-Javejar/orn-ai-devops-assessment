e82d7add3c24bc4b50077c200a9be4151c5d4387
Tejashwini-Javejar <tejujave2908@gmail.com>
2026-07-05 14:04:20 +0530

    Add IAM, security group, and EKS modules

--- Capstone Project/Terraform/main.tf
+++ Capstone Project/Terraform/main.tf
@@ -1,6 +1,21 @@
 module "vpc" {
-
   source = "./modules/vpc"
+}
 
-  vpc_cidr = "10.0.0.0/16"
+module "iam" {
+  source = "./modules/iam"
+}
+
+module "security_group" {
+  source = "./modules/security-group"
+  vpc_id = module.vpc.vpc_id
+}
+
+module "eks" {
+  source = "./modules/eks"
+
+  cluster_name = var.cluster_name
+  subnet_ids   = module.vpc.private_subnets
+
+  cluster_role_arn = module.iam.cluster_role_arn
 }
