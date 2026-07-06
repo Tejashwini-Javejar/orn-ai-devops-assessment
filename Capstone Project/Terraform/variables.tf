077c6c01b283d93e2953411cdfe66f775c50007c
Tejashwini-Javejar <tejujave2908@gmail.com>
2026-07-06 20:19:14 +0530

    Update variables.tf

--- Capstone Project/Terraform/variables.tf
+++ Capstone Project/Terraform/variables.tf
@@ -1,43 +1,31 @@
 variable "aws_region" {
-  description = "AWS region"
-  type        = string
-  default     = "us-east-1"
+  default = "us-east-1"
 }
 
 variable "cluster_name" {
-  description = "EKS cluster name"
-  type        = string
-  default     = "capstone-eks-cluster"
+  default = "capstone-eks-cluster"
 }
 
 variable "vpc_name" {
-  description = "VPC name"
-  type        = string
-  default     = "myvpc"
+  default = "myvpc"
 }
 
 variable "vpc_cidr" {
-  type    = string
   default = "10.0.0.0/16"
 }
 
 variable "public_subnet_cidrs" {
-  type    = list(string)
   default = ["10.0.1.0/24", "10.0.2.0/24"]
 }
 
 variable "private_subnet_cidrs" {
-  type    = list(string)
   default = ["10.0.3.0/24", "10.0.4.0/24"]
 }
 
 variable "availability_zones" {
-  description = "Manually defined AZs (no data source used)"
-  type        = list(string)
-  default     = ["us-east-1a", "us-east-1b"]
+  default = ["us-east-1a", "us-east-1b"]
 }
 
 variable "instance_type" {
-  type    = string
   default = "m7i-flex.large"
 }
