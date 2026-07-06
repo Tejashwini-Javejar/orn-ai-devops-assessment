7d51a25bcd6cf3f1840b2da0289ad5456b6b7ac1
Tejashwini-Javejar <tejujave2908@gmail.com>
2026-07-06 17:59:45 +0530

    Add new variables for AWS infrastructure

--- Capstone Project/Terraform/variables.tf
+++ Capstone Project/Terraform/variables.tf
@@ -1,5 +1,37 @@
+variable "aws_region" {
+  description = "AWS region"
+  type        = string
+  default     = "us-east-1"
+}
+
 variable "cluster_name" {
+  description = "EKS cluster name"
+  type        = string
+  default     = "capstone-eks"
+}
 
-  default = "gitops-eks"
+variable "vpc_name" {
+  description = "VPC name"
+  type        = string
+  default     = "myvpc"
+}
 
+variable "vpc_cidr" {
+  type    = string
+  default = "10.0.0.0/16"
+}
+
+variable "public_subnets" {
+  type    = list(string)
+  default = ["10.0.1.0/24", "10.0.2.0/24"]
+}
+
+variable "private_subnets" {
+  type    = list(string)
+  default = ["10.0.10.0/24", "10.0.11.0/24"]
+}
+
+variable "instance_type" {
+  type    = string
+  default = "t3.micro"
 }
