f6919576100b433bd28a326054468745e31c8fb2
Tejashwini-Javejar <tejujave2908@gmail.com>
2026-07-06 18:08:45 +0530

    Refactor EKS outputs in outputs.tf

--- Capstone Project/Terraform/outputs.tf
+++ Capstone Project/Terraform/outputs.tf
@@ -1,15 +1,15 @@
-output "cluster_name" {
-  value = aws_eks_cluster.cluster.name
-}
-
 output "vpc_id" {
   value = aws_vpc.myvpc.id
 }
 
-output "node_group" {
-  value = aws_eks_node_group.nodes.node_group_name
+output "cluster_name" {
+  value = aws_eks_cluster.eks_cluster.name
 }
 
 output "cluster_endpoint" {
-  value = aws_eks_cluster.cluster.endpoint
+  value = aws_eks_cluster.eks_cluster.endpoint
+}
+
+output "node_group_name" {
+  value = aws_eks_node_group.nodes.node_group_name
 }
