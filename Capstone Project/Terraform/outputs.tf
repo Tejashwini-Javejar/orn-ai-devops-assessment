c4ac38f3ebd481723a14aaefc6ff7935670da7e3
Tejashwini-Javejar <tejujave2908@gmail.com>
2026-07-06 20:24:59 +0530

    Update outputs.tf

--- Capstone Project/Terraform/outputs.tf
+++ Capstone Project/Terraform/outputs.tf
@@ -1,11 +1,3 @@
-output "vpc_id" {
-  value = aws_vpc.main.id
-}
-
-output "vpc_name" {
-  value = var.vpc_name
-}
-
 output "cluster_name" {
   value = aws_eks_cluster.cluster.name
 }
@@ -14,10 +6,6 @@ output "cluster_endpoint" {
   value = aws_eks_cluster.cluster.endpoint
 }
 
-output "public_subnets" {
-  value = aws_subnet.public[*].id
-}
-
-output "private_subnets" {
-  value = aws_subnet.private[*].id
+output "vpc_id" {
+  value = aws_vpc.main.id
 }
